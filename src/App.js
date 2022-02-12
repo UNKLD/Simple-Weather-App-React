d6f6e2c5024d107211c59987ef91f313d603673a
UNKLD <leuldamtew@yahoo.com>
2022-02-12 07:01:18 -0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [locations, setLocations] = useState("dubai");
+  const [weatherData, setWeatherData] = useState([]);
+  const [photos, setPhotos] = useState([]);
+
+  const weather = async () => {
+    // Fetching weather Data
+    try {
+      const res = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${locations}&APPID=6f29b335b91ab50b166193a109b60c43&units=metric`
+      );
+      const data = await res.json();
+      setWeatherData(await data);
+    } catch (error) {
+      console.log(error);
+    }
+    //Fetching location Image
+    try {
+      const res = await fetch(
+        `https://api.unsplash.com/search/photos?query=${locations}&client_id=dtWfpSQIjfK1LeekqsEA_Gz_w7OWfVxwOheNYgVneHw`
+      );
+      const data = res.ok && (await res.json());
+      setPhotos(data?.results[0]?.urls?.raw);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleClick = () => {
+    weather();
+  };
+
+  useEffect(() => {
+    weather();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {weatherData && weatherData.weather && (
+        <header className="App-header">
+          <div className="findCity">
+            <input
+              type="text"
+              required
+              value={locations}
+              onChange={(e) => setLocations(e.target.value)}
+            />
+            <button onClick={handleClick}>Submit</button>
+          </div>
+          <div className="card">
+            <div className="card-img">
+              <img height={200} width={300} src={photos} alt={photos.toString()} />
+            </div>
+            <div className="card-info">
+              <div>
+                <p>City : {weatherData.name}</p>
+                <p>Weather : {weatherData.weather[0].description}</p>
+                <p>
+                  Temprature : {weatherData.main.temp} <span>&#176;</span>C
+                </p>
+              </div>
+            </div>
+            <div className="card-icon">
+              <img
+                src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`}
+                width={200}
+                height={200}
+                alt={weatherData.weather[0].description}
+              />
+            </div>
+          </div>
+        </header>
+      )}
     </div>
   );
 }
